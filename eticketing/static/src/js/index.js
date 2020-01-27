922f59e6b87756595ab39cefd7270e3415992c78
Sébastien Vallet <sebastien.vallet89@gmail.com>
2020-01-27 14:16:34 +0100

    added font-awesome in webpack

--- eticketing/static/src/js/index.js
+++ eticketing/static/src/js/index.js
@@ -1,5 +1,11 @@
 // add your files to build into entry files app.css / app.bundle.js
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
+
 import "../img/logo.png";
 import "../scss/main.scss";
