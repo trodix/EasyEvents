d4be8a8735a711c502c9bb287fdf8019f36b02b5
Sébastien Vallet <sebastien.vallet89@gmail.com>
2020-02-02 16:24:10 +0100

    added some style

--- EasyEvents/settings.py
+++ EasyEvents/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'crispy_forms',
 
     'eticketing'
 ]
@@ -148,3 +149,7 @@ AUTHENTICATION_BACKENDS = (
 )
 
 SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
